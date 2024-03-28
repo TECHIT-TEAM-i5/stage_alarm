6b866ea62b1769f7b9e47877aa8e6e57e8937430
fish-minkyu <e951219@naver.com>
2024-03-28 17:15:23 +0900

    Feat: ing

--- src/main/java/com/example/stagealarm/board/controller/BoardController.java
+++ src/main/java/com/example/stagealarm/board/controller/BoardController.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/boards")
+@RequestMapping("/board")
 @RequiredArgsConstructor
 public class BoardController {
   private final BoardService boardService;
--- src/main/java/com/example/stagealarm/board/controller/BoardViewController.java
+++ src/main/java/com/example/stagealarm/board/controller/BoardViewController.java
@@ -1,13 +1,15 @@
 package com.example.stagealarm.board.controller;
 
-import com.example.stagealarm.board.dto.BoardDto;
-import com.example.stagealarm.board.service.BoardService;
-import com.example.stagealarm.board.service.CategoryService;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@Controller
+@RequestMapping("/boards")
 public class BoardViewController {
+  @GetMapping
+  public String boardView() {
+    return "content/board/test";
+  }
 }
